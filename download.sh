7cfb1dc494fee7c98fd00f8b94ed8d4c35e409da
guardianwang <962247283@qq.com>
2022-05-14 12:25:26 -0400

    download checkpoint.pth

---
+++ download.sh
@@ -0,0 +1,8 @@
+gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+
+echo "Downloading dataset..."
+gdrive_download 1cEWTQo8jXz5krJ3clViH70rDFnR4-8Sh checkpoint.pth
